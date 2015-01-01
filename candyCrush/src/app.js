b64e832b3ff7b35038bc46b218239c0a0d6f83b3
e-vasyl <e.vasyl@gmail.com>
2015-01-01 14:50:26 +0200

    add mouse listener

--- candyCrush/src/app.js
+++ candyCrush/src/app.js
@@ -1,3 +1,16 @@
+var hoverHandler = cc.EventListener.create({
+    event: cc.EventListener.MOUSE,
+    
+    
+    onMouseMove: function (event) {
+      var target = event.getCurrentTarget();
+      var locationInNode = target.convertToNodeSpace(event.getLocation());
+      var idx = target.idxFromPos(locationInNode);
+      target.selectCell(locationInNode);
+      return true;
+  }
+});
+
 var BoardLayer = cc.Layer.extend({
     board:[],
     
@@ -25,21 +38,55 @@ var BoardLayer = cc.Layer.extend({
             };
     },
     
-    addCandy:function(clr, pos)
+    idxFromPos:function(pos){
+        return {
+            i: Math.floor((pos.x) / 42),
+            j: Math.floor((pos.y - 40) / 42)
+        }
+    },
+    
+    isOnBoard:function(idx){
+          return ((idx.i >= 0) && (idx.j >= 0) && 
+                  (idx.i < this.board.length) && (idx.j < this.board.length));
+    },
+
+    selectedIdx:{i:-1,j:-1},
+    selectCell:function(pos){
+        var idx = this.idxFromPos(pos);
+        var oidx = this.selectedIdx;
+        if ((oidx.i != idx.i) || (oidx.j != idx.j))
+        {
+            if (oidx.j != -1)
+            {
+                this.board[oidx.i][oidx.j].opacity = 255;
+            }
+            if (this.isOnBoard(idx))
+            {
+                this.board[idx.i][idx.j].opacity = 180;
+                this.selectedIdx = idx;
+            }
+            else
+            {
+                this.selectedIdx = {i:-1,j:-1};
+            }
+        }
+    },
+    
+    addCandy:function(type, pos)
     {
         var candy = new cc.LabelTTF("*", "Arial", 146);
-        // position the label on the center of the screen
         candy.x = pos.x;
         candy.y = pos.y;
-        candy.color = clr;
-        // add the label as a child to this layer
+        candy.color = type.clr;
+        // set custom properties
+        candy.candyType = type.t;
         this.addChild(candy, 0);        
         return candy;
     },
     
     getCandyType:function(){
         var i = Math.floor(Math.random() * this.candies.length);
-        return this.candies[i];
+        return {t: i, clr: this.candies[i]};
     },
     
     initBoard:function(size){
@@ -52,6 +99,9 @@ var BoardLayer = cc.Layer.extend({
                 this.board[i].push(this.addCandy(clr, pos));
             }
         }
+        
+        cc.eventManager.addListener(hoverHandler.clone(), this);
+        
     },
     
 });
