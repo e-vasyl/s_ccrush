8206ef860c99e60bb2786de5dcb30ca2812348d4
e-vasyl <e.vasyl@gmail.com>
2015-01-01 13:32:04 +0200

    limit candy initial types to 6 colors

--- candyCrush/src/app.js
+++ candyCrush/src/app.js
@@ -1,6 +1,16 @@
 var BoardLayer = cc.Layer.extend({
     board:[],
     
+    candies:[
+            cc.color("#ff8080"),
+            cc.color("#80ff80"),
+            cc.color("#8080ff"),
+ 
+            cc.color("#ff80ff"),
+            cc.color("#80ffff"),
+            cc.color("#ffff80"),
+            ],
+    
     ctor:function(){
         this._super();
         
@@ -10,8 +20,8 @@ var BoardLayer = cc.Layer.extend({
 
     posFromCoord:function(i, j){
         return {
-            x: i*40 + 40,
-            y: j*40 + 40
+            x: i*42 + 20,
+            y: j*42 + 20
             };
     },
     
@@ -27,13 +37,18 @@ var BoardLayer = cc.Layer.extend({
         return candy;
     },
     
+    getCandyType:function(){
+        var i = Math.floor(Math.random() * this.candies.length);
+        return this.candies[i];
+    },
+    
     initBoard:function(size){
         this.board = [];
         for (var i = 0; i<size; i++){
             this.board.push([]);
             for (var j = 0; j<size; j++){
                 var pos = this.posFromCoord(i, j);
-                var clr = cc.color(Math.random()*255, 128, 128);
+                var clr = this.getCandyType();
                 this.board[i].push(this.addCandy(clr, pos));
             }
         }
